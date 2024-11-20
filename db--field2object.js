b7475aaef01ff4f8dfc127b14db3af629418db61
David Jarvis <ddjarvis@up.edu.ph>
2024-11-19 20:33:25 -0500

    Add files via upload

---
+++ db--field2object.js
@@ -0,0 +1,41 @@
+/*
+=====================================================================
+	Field2Object - Create/update embedded objects with data from fields
+	e    = Entry Object
+	o    = Object Field Name
+	args = Prop/Field Names Array
+	opt  = Additional Options Object
+	opt.delim = Delimiter Str // Default: |
+	opt.clear = Clear Src Bool // Default: false
+	opt.index = Target Index // Default: object length
+=====================================================================
+*/
+
+function field2object(e,o,args,opt) {
+	if(!e || !o) { exit(); }
+	if(!args || args.length==0) { exit(); }
+	
+	var f = e.field(o);
+	var d = opt.delim || "|";
+	var c = (opt.clear===undefined) ? false : opt.clear;
+	var i = (!!opt.index && opt.index<f.length) ? opt.index : f.length;
+	
+	var obj = {};
+	var arg, chk, key, val;
+	var reg = new RegExp("(.*) *["+d+"] *(.*)");
+	for(var x=0; x<args.length; x++){
+		arg = args[x];
+		chk = !!arg.match(reg);
+		if(chk) {
+			[chk, val, key] = arg.match(reg);
+		} else {
+			key = arg;
+			val = arg;
+		}
+		obj[key] = e.field(val);
+		if(c) { e.set(val,''); }
+	}
+	f[i] = obj;
+	e.set(o, f);
+	e.recalc();
+}
