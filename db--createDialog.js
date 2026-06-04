763191218c2994a638110a14c5a92d89448a94ab
David Jarvis <ddjarvis@up.edu.ph>
2026-06-05 00:10:47 +0800

    Update db--createDialog.js

--- db--createDialog.js
+++ db--createDialog.js
@@ -59,7 +59,6 @@ function createDialog(title, text, posi, nega, neut) {
 	if(!!posi){ myDialog.positiveButton(posi.text, posi.fn); }
 	if(!!nega){ myDialog.negativeButton(nega.text, nega.fn); }
 	if(!!neut){ myDialog.neutralButton(neut.text, neut.fn); }
-	let ret = myDialog.show();
-	log(`createDialog: ${ret}`);
-return ret;
+	myDialog.show();
+	return true;
 }
